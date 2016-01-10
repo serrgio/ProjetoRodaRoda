db27152ef43fe55cf1e6908fb1ce1a6bb03bdb11
GAOliveira <GAOliveira@GSA07.gsa.go>
2016-01-09 22:40:41 -0200

    Criação do projeto Roda Roda

---
+++ src/jogorodaroda/Classe/Main.java
@@ -0,0 +1,14 @@
+package jogorodaroda.Classe;
+
+import jogorodaroda.Interface.TelaInicial;
+
+public class Main {
+    public static void main(String args[]) {
+        TelaInicial telaInicial = new TelaInicial();
+
+        telaInicial.dispose();
+        telaInicial.setUndecorated(true);
+        telaInicial.setVisible(true);
+        telaInicial.setLocationRelativeTo(null);
+    }
+}
